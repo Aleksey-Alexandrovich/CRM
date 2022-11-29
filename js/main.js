5b066e660714699e1608a6f538c762f3b0dd326a
Aleksey <alexeyegorov1987@mail.ru>
2022-11-29 20:13:44 +0300

    add new files

---
+++ js/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const brand = 'Sony';
+
+const count = 10;
+
+const category = 'Television';
+
+const price = 639;
+
+console.log(brand);
+
+
+const total = count * price;
+
+console.log(total);
