ac1cf935e1fb7a08814b03fd17387df83be8cc2e
Aleksey <alexeyegorov1987@mail.ru>
2022-12-05 21:35:32 +0300

    add new files

---
+++ js/einstein.js
@@ -0,0 +1,8 @@
+
+const myWeight = 87;
+
+const speedlight = 3e8;
+
+const energy = myWeight * (speedlight * speedlight);
+
+console.log(energy);
--- js/main.js
+++ js/main.js
@@ -14,3 +14,23 @@ console.log(brand);
 const total = count * price;
 
 console.log(total);
+
+const newBrand = prompt('Введите наименование товара:');
+
+const newCount = prompt('Введите количество товара:');
+
+
+const newCategory = prompt('Введите категорию товара:');
+
+const newPrice = prompt('Введите цену товара:');
+
+console.log(typeof newBrand);
+console.log(typeof +newCount);
+console.log(typeof newCategory);
+console.log(typeof +newPrice);
+console.log(`На складе ${newCount} единицы товара ${newBrand} на сумму ${newPrice} деревянных`);
+
+
+
+
+
