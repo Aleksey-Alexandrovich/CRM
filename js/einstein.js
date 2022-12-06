dc7ff3debf9c4f2b5943ca8a1763714ebbee955d
Aleksey <alexeyegorov1987@mail.ru>
2022-12-06 22:22:04 +0300

    add new files

--- js/einstein.js
+++ js/einstein.js
@@ -1,4 +1,6 @@
 
+'use stricrt';
+
 const myWeight = 87;
 
 const speedlight = 3e8;
@@ -6,3 +8,21 @@ const speedlight = 3e8;
 const energy = myWeight * (speedlight * speedlight);
 
 console.log(energy);
+
+
+
+
+// let games = ' ';
+
+// if(games === 'Playstation') {
+// 	console.log('Ты сонибой))');
+
+// } 
+
+// if(games === 'Xbox') {
+// 	console.log('Ты майкобой))');
+// }
+
+// if (games === ' '){
+// 	console.log('Ты бич');
+// }
--- js/main.js
+++ js/main.js
@@ -1,34 +1,32 @@
 'use strict';
 
-const brand = 'Sony';
+{
+	const brand = 'Sony';
+	const count = 10;
+	const category = 'Television';
+	const price = 639;
+	const total = count * price;
 
-const count = 10;
-
-const category = 'Television';
-
-const price = 639;
-
-console.log(brand);
-
-
-const total = count * price;
-
-console.log(total);
-
-const newBrand = prompt('Введите наименование товара:');
-
-const newCount = prompt('Введите количество товара:');
+	console.log(brand);
+	console.log(total);
 
 
-const newCategory = prompt('Введите категорию товара:');
+}
 
-const newPrice = prompt('Введите цену товара:');
+{
+	
+	const brand = prompt('Введите наименование товара:');
+	const count = +prompt('Введите количество товара:');
+	const category = prompt('Введите категорию товара:');
+	const price = +prompt('Введите цену товара:');
 
-console.log(typeof newBrand);
-console.log(typeof +newCount);
-console.log(typeof newCategory);
-console.log(typeof +newPrice);
-console.log(`На складе ${newCount} единицы товара ${newBrand} на сумму ${newPrice} деревянных`);
+	console.log(typeof brand);
+	console.log(typeof +count);
+	console.log(typeof category);
+	console.log(typeof +price);
+	console.log(`На складе ${count} единицы товара ${brand} на сумму ${price} деревянных`);
+
+}
 
 
 
