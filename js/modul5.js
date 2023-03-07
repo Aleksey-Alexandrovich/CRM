a836242fa97f39224bbe94a133be13ca4365b773
Alexey <84179953+Aleksey-Alexandrovich@users.noreply.github.com>
2023-03-07 16:26:46 +0300

    Добавили js файл и создали константы

---
+++ js/modul5.js
@@ -0,0 +1,15 @@
+'use strict';
+const header = document.querySelector('.modal__title'); //Заголовок 
+
+const summaryButtton = document.querySelector('.summary__button'); //Кнопка возле id и сам id - Я только в этом сомневаюсь что за id и сам id ?
+
+const closeModal = document.querySelector('.modal__btn--button'); //Кнопка возле id и сам id -  Я только в этом сомневаюсь что за id и сам id ?
+
+
+const form = document.querySelector('.form'); // Форма
+
+const formCheckbox = document.querySelector('.form__checkbox'); // Чекбокс
+
+const formCheckboxField = document.querySelector('.form__iput--short'); //Поле рядом с чекбоксом
+
+const summaryCost = document.querySelector('.summary__cost'); //Итоговую стоимость
